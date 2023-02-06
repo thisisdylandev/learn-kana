49b19e2a87fd6aa80b3e117e642d8604d8af4a4c
Dylan <dgreen@hey.com>
2023-02-06 21:56:29 +0000

    update home page, use next link & image

--- src/app/katakana/page.tsx
+++ src/app/katakana/page.tsx
@@ -1,6 +1,8 @@
 "use client"; // this is a client component
 
 import { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import Katakana from '../katakana.json'
 import shuffle from '../functions/shuffle'
 import NonSSRWrapper from '../components/no-ssr-wrapper'
@@ -40,13 +42,20 @@ function Home() {
   return (
     <NonSSRWrapper>
       <header className="text-right mb-8 mr-6">
+        <Link href="/" className="mx-auto">HOME</Link>
         <p>Correct: {correct}</p>
         <p>Incorrect: {incorrect}</p>
         <p>Remaining: {remaining}</p>
       </header>
       <section className="min-w-full">
         <div className="mx-auto px-10 py-5 bg-white border border-gray-200 rounded-lg shadow max-w-sm">
-          <img src={`./katakana/${currentKatakana.romanization}.svg`} className="mb-4 max-h-64 max-w-64"/>
+        <Image
+              src={`/katakana/${currentKatakana.romanization}.svg`}
+              className="mb-4 max-h-64 max-w-64"
+              alt=""
+              width={500}
+              height={500}
+            />
           <form onSubmit={(e) => {updateKana(e)}}>
             <input type="text" name="kana" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-auto"/>
           </form>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,19 @@
-"use client"; // this is a client component
-
-import { useState } from 'react'
-import Hiragana from './hiragana.json'
-import shuffle from './functions/shuffle'
-import NonSSRWrapper from './components/no-ssr-wrapper'
+import Link from 'next/link'
 
 function Home() {
-  const shuffledHiragana = shuffle(Hiragana);
-  const [hiraganaArray, setHiraganaArray] = useState(shuffledHiragana);
-  const [currentHiragana, setCurrentHiragana] = useState(hiraganaArray[0]);
-  const [correct, setCorrect] = useState(0);
-  const [incorrect, setIncorrect] = useState(0);
-  const [remaining, setRemaining] = useState(Hiragana.length);
-
-  const updateKana = (e: React.FormEvent) => {
-    e.preventDefault();
-    let newHiraganaArray = hiraganaArray;
-    let userInput: string = (e.target as HTMLFormElement).kana.value;
-    if (userInput === currentHiragana.romanization) {
-        newHiraganaArray.shift();
-        newHiraganaArray = shuffle(newHiraganaArray);
-        setHiraganaArray(newHiraganaArray);
-        setCurrentHiragana(newHiraganaArray[0]);
-        setCorrect(correct => correct + 1);
-        setRemaining(remaining => remaining - 1);
-        (e.target as HTMLFormElement).reset();
-    } else {
-        const incorrectFlashcard = newHiraganaArray[0];
-        newHiraganaArray.shift();
-        newHiraganaArray.push(incorrectFlashcard);
-        newHiraganaArray = shuffle(newHiraganaArray);
-        setIncorrect(incorrect => incorrect + 1);
-        setHiraganaArray(newHiraganaArray);
-        setCurrentHiragana(newHiraganaArray[0]);
-        (e.target as HTMLFormElement).reset();
-    }
-  };
 
   return (
-    <NonSSRWrapper>
-      <section className="min-w-full">
-        <div className="mx-auto px-10 py-5 bg-white border border-gray-200 rounded-lg shadow max-w-sm">
-          <img src={`./hiragana/${currentHiragana.char_id}`} className="mb-4 max-h-64 max-w-64"/>
-          <form onSubmit={(e) => {updateKana(e)}}>
-            <input type="text" name="kana" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-auto"/>
-          </form>
+    <>
+      <section className="min-w-full flex h-screen">
+        <div className="m-auto px-10 py-5 bg-white border border-gray-200 rounded-lg shadow">
+          <div className="grid grid-cols-2 grid-rows-2">
+            <h1 className="text-lg font-bold col-span-2">Would you like to practice hiragana or katakana?</h1>
+            <Link href="/hiragana" className="mx-auto">HIRAGANA</Link>
+            <Link href="/katakana" className="mx-auto">KATAKANA</Link>
+          </div>
         </div>
       </section>
-    </NonSSRWrapper>
+    </>
   )
 }
 
