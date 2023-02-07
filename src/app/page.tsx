67a1e3c670babee40798243356bbe8d0d35084cd
Dylan <dgreen@hey.com>
2023-02-07 22:32:16 +0000

    update katakana json to use character over of kana

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,120 @@
-import Link from 'next/link'
+"use client"; // this is a client component
+
+import { useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import Header from './components/header'
 
 function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [kanaType, setKanaType] = useState('hiragana');
+  const [columns, setColumns] = useState<string[]>([]);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const headerProps = {
+    correct: searchParams.get('correct') || "0",
+    incorrect: searchParams.get('incorrect') || "0",
+    remaining: searchParams.get('remaining') || "0",
+  }
+
+  const handleCheckboxClick = (column: string) => {
+    let queryColumns = columns
+    if (queryColumns.includes(column)){
+      queryColumns = queryColumns.filter(item => item !== column)
+    } else {
+      queryColumns.push(column)
+    }
+    setIsDisabled(!(queryColumns.length > 0))
+    setColumns(queryColumns)
+  }
+
+  const handleButtonClick = () => {
+    let query = `kana=${kanaType}&columns=`
+    columns.forEach(item => query = query + `${item},`)
+    // remove trailing comma
+    query = query.substring(0, query.length - 1);
+    router.push(`quiz?${query}`)
+  }
 
   return (
     <>
+      <Header {...headerProps}/>
       <section className="min-w-full flex h-screen">
         <div className="m-auto px-10 py-5 bg-white border border-gray-200 rounded-lg shadow">
-          <div className="grid grid-cols-2 grid-rows-2">
+          <div className="grid grid-cols-2 gap-x-7">
             <h1 className="text-lg font-bold col-span-2">Would you like to practice hiragana or katakana?</h1>
-            <Link href="/hiragana" className="mx-auto">HIRAGANA</Link>
-            <Link href="/katakana" className="mx-auto">KATAKANA</Link>
+            <div className="col-span-2 mx-auto">
+              <div className="form-control mx-auto">
+                <label className="label cursor-pointer">
+                  <input type="radio" name="kana" checked className="radio mr-5" onChange={() => setKanaType('hiragana')} />
+                  <span className="label-text">HIRAGANA</span> 
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <input type="radio" name="kana" className="radio mr-5" onChange={() => setKanaType('katakana')} />
+                  <span className="label-text">KATAKANA</span> 
+                </label>
+              </div>
+            </div>
+            <h2 className="text-lg font-bold col-span-2 mx-auto">Which columns would you like to study?</h2>
+            <div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Root (a,i,o,e,u)</span> 
+                  <input type="checkbox" className="checkbox" name="root" onChange={() => handleCheckboxClick('root')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">K</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('k')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">S</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('s')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">T</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('t')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">N</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('n')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Combo</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('combo')}/>
+                </label>
+              </div>
+            </div>
+            <div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">H</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('h')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">M</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('m')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Y</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('y')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">R</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('r')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">W/-N</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('w')}/>
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Variants</span> 
+                  <input type="checkbox" className="checkbox" onChange={() => handleCheckboxClick('variants')}/>
+                </label>
+              </div>
+            </div>
+            <button className="btn btn-wide col-span-2 mt-5 mx-auto" id="btn_begin" disabled={isDisabled} onClick={() => handleButtonClick()}>BEGIN</button>
           </div>
         </div>
       </section>
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,123 @@
+"use client"; // this is a client component
+
+import { useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import Image from 'next/image'
+import Hiragana from '../hiragana.json'
+import Katakana from '../katakana.json'
+import shuffle from '../functions/shuffle'
+import Header from '../components/header'
+import NonSSRWrapper from '../components/no-ssr-wrapper'
+
+function Quiz() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const kana = searchParams.get('kana') === 'hiragana' ? Hiragana : Katakana
+  
+  // TODO: use all columns if columns query param is missing
+  const columns = searchParams!.get('columns')!.split(',')
+  let kanaQuiz: any[] = []
+  if (columns.includes("root")) {
+    kana.forEach(char => {if (char.romanization.length === 1) kanaQuiz.push(char) });
+    // remove 'n' from root kana
+    kanaQuiz = kanaQuiz.filter(item => !(item.romanization === 'n'))
+  }
+
+  columns.forEach(item => {
+    kana.forEach(char => {if (char.romanization.startsWith(item)) kanaQuiz.push(char) });
+  })
+  if (columns.includes("variants")) {
+    kana.forEach(char => {
+      if (char.romanization.startsWith('d') || char.romanization.startsWith('g') || char.romanization.startsWith('z') || char.romanization.startsWith('j') || char.romanization.startsWith('b') || char.romanization.startsWith('p')) {
+        kanaQuiz.push(char)
+      }
+    });
+  }
+  kanaQuiz = kanaQuiz.filter(item => !(item.character.length === 2))
+
+  if (columns.includes("h")) {
+    // @ts-ignore
+    const found = kana.filter(item => item.romanization === 'fu');
+    kanaQuiz.push(found[0])
+  }
+  if (columns.includes("t")) {
+    // @ts-ignore
+    const found = kana.filter(item => item.romanization === 'chi');
+    kanaQuiz.push(found[0])
+  }
+
+  if (columns.includes("combo")) { kana.forEach(char => {if (char.character.length >= 2) kanaQuiz.push(char) });}
+
+  // @ts-ignore
+  // TODO: fix the ts error on line below
+  const shuffledKana = shuffle(kanaQuiz);
+
+  const [kanaArray, setKanaArray] = useState(shuffledKana);
+  const [currentKana, setCurrentKana] = useState(kanaArray[0]);
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [remaining, setRemaining] = useState(kanaQuiz.length);
+  let imageSrc = ''
+  if (currentKana === undefined) {
+    imageSrc = `/hiragana/a.svg`
+  } else if (kana === Hiragana){
+    imageSrc = `/hiragana/${currentKana.char_id}`
+  } else {
+    imageSrc = `/katakana/${currentKana.romanization}.svg`
+  }
+
+  const updateKana = (e: React.FormEvent) => {
+    e.preventDefault();
+    let newKanaArray = kanaArray;
+    const testArray = Array.from(kanaArray);
+    testArray.shift()
+    if (testArray.length === 0){ router.push(`/?correct=${correct.toString()}&incorrect=${incorrect.toString()}&remaining=0`) }
+    let userInput: string = (e.target as HTMLFormElement).kana.value;
+    if (userInput === currentKana.romanization) {
+        newKanaArray.shift();
+        newKanaArray = shuffle(newKanaArray);
+        setKanaArray(newKanaArray);
+        setCurrentKana(newKanaArray[0]);
+        setCorrect(correct => correct + 1);
+        setRemaining(remaining => remaining - 1);
+        (e.target as HTMLFormElement).reset();
+    } else {
+        const incorrectFlashcard = newKanaArray[0];
+        newKanaArray.shift();
+        newKanaArray.push(incorrectFlashcard);
+        newKanaArray = shuffle(newKanaArray);
+        setIncorrect(incorrect => incorrect + 1);
+        setKanaArray(newKanaArray);
+        setCurrentKana(newKanaArray[0]);
+        (e.target as HTMLFormElement).reset();
+    }
+  };
+
+  const headerProps = {
+    correct: correct.toString(),
+    incorrect: incorrect.toString(),
+    remaining: remaining.toString(),
+  }
+
+  return (
+    <NonSSRWrapper>
+      <Header {...headerProps} />
+      <section className="min-w-full">
+        <div className="mx-auto px-10 py-5 bg-white border border-gray-200 rounded-lg shadow max-w-sm">
+          <Image
+              src={imageSrc}
+              className="mb-4 max-h-64 max-w-64"
+              alt="Japanese Kana"
+              width={500}
+              height={500}
+            />
+          <form onSubmit={(e) => {updateKana(e)}}>
+            <input type="text" name="kana" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-auto"/>
+          </form>
+        </div>
+      </section>
+    </NonSSRWrapper>
+  )
+}
+
+export default Quiz
